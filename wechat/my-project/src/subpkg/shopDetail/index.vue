b9d87bd2b2655f64d926b8bfee93220509b079cb
13damowang <597638751@qq.com>
2023-04-18 23:32:48 +0800

    商品详情页面开发

--- wechat/my-project/src/subpkg/shopDetail/index.vue
+++ wechat/my-project/src/subpkg/shopDetail/index.vue
@@ -1,19 +1,123 @@
 <template>
-  <div class="shopDetail">shopDetail</div>
+  <view v-if="Req.goods_price" class="shopDetailContainer">
+    <swiper
+      indicator-dots
+      autoplay
+      circular
+    >
+      <swiper-item v-for="(item, index) in Req.pics" :key="index">
+        <image
+          :src="item.pics_big"
+          mode="scaleToFill"
+          @click="preview(index)"
+        />
+      </swiper-item>
+    </swiper>
+    <view class="shop-info-box">
+      <view class="shop-price">¥{{ Req.goods_price }}</view>
+      <view class="shop-tip">
+        <view class="tip-content">{{Req.goods_name}}</view>
+        <view class="collect">
+          <uni-icons type="star" size="20"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <view class="express">
+        快递：免运费
+      </view>
+    </view>
+    <rich-text :nodes="Req.goods_introduce"></rich-text>
+
+    <view class="shop-nav">
+      <uni-goods-nav :fill="true" :options="options" :button-group="buttonGroup" @click="onClick" @buttonClick="buttonClick"></uni-goods-nav>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return {}
+    return {
+      Req: {},
+      id: '',
+      options: [
+        {
+          icon: 'shop',
+          text: '店铺',
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }
+      ],
+      buttonGroup: [
+        {
+          text: '加入购物车',
+          backgroundColor: '#ff0000',
+          color: '#fff'
+        },
+        {
+          text: '立即购买',
+          backgroundColor: '#ffa200',
+          color: '#fff'
+        }
+	    ]
+    }
   },
   computed: {},
-  methods: {},
+  methods: {
+    getShopDeatil () {
+      uni.showLoading({
+        title: '数据加载中'
+      })
+      uni.request({
+        url: `${uni.$beseUrl}/api/public/v1/goods/detail`,
+        data: {
+          goods_id: this.id
+        },
+        success: (data) => {
+          const {data: res} = data
+          console.log(res)
+          if (res.meta.status !== 200) {
+            return uni.$showMeg('数据加载失败！')
+          }
+          res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display: block;"').replace(/webp/g, 'jpg')
+          this.Req = res.message
+        },
+        complete: () => {
+          uni.hideLoading()
+        }
+      });
+    },
+
+    preview (index) {
+      uni.previewImage({
+        urls: this.Req.pics.map((item) => item.pics_big),
+        current: index
+      });
+    },
+
+    onClick (e) {
+      if (e.content.text === '购物车') {
+        uni.switchTab({
+           url: '/pages/cart/index'
+        });
+      }
+    },
+
+    buttonClick (e) {
+
+    }
+
+  },
   watch: {},
 
   // 页面周期函数--监听页面加载
-  onLoad() {},
+  onLoad(options) {
+    this.id = options.good_id
+    this.getShopDeatil()
+  },
   // 页面周期函数--监听页面初次渲染完成
   onReady() {},
   // 页面周期函数--监听页面显示(not-nvue)
@@ -33,4 +137,61 @@ export default {
 } 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.shopDetailContainer {
+  padding-bottom: 50px;
+
+  swiper {
+    height: 750rpx;
+  
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  
+  .shop-info-box {
+    padding: 10px;
+    .shop-price {
+      color: red;
+      font-size: 18px;
+      margin: 10px 0;
+    }
+  
+    .shop-tip {
+      display: flex;
+      justify-content: space-between;
+  
+      .tip-content {
+        font-size: 13px;
+        margin-right: 10px;
+      }
+  
+      .collect {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: gray;
+        border-left: 1px solid #eee;
+      }
+    }
+  
+    .express {
+      color: gray;
+      font-size: 12px;
+      margin: 10px 0;
+    }
+  
+  }
+  
+  .shop-nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
